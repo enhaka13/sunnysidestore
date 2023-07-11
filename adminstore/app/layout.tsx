fd2556f72f527a0ae52593a9cd5ba7c468113ac6
Naufal Hanif Kusuma <68142599+enhaka13@users.noreply.github.com>
2023-07-11 16:45:51 +0000

    just make sure

--- adminstore/app/layout.tsx
+++ adminstore/app/layout.tsx
@@ -3,23 +3,35 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Admin Dashboard',
-  description: 'Admin Dashboard',
-}
+// export const metadata = {
+//   title: 'Admin Dashboard',
+//   description: 'Admin Dashboard',
+// }
 
-export default function RootLayout({
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <ClerkProvider>
+//       <html lang="en">
+//         <body className={inter.className}>{children}</body>
+//       </html>
+//     </ClerkProvider>
+//   )
+// }
+
+export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <div className="flex items-center justify-center h-full w-full">
+      {children}
+    </div>
   )
 }
